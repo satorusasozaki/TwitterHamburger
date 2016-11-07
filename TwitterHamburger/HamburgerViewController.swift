0d4190b57b0853cb36640083e941dfcb2cb39e8d
satorusasozaki <sasozakisatoru@gmail.com>
2016-11-07 09:31:23 -0800

    Add slider view

--- TwitterHamburger/HamburgerViewController.swift
+++ TwitterHamburger/HamburgerViewController.swift
@@ -9,8 +9,33 @@
 import UIKit
 
 class HamburgerViewController: UIViewController {
+    @IBOutlet weak var MenuView: UIView!
+    @IBOutlet weak var ContentView: UIView!
+    var originalContentLeftMargin: CGFloat!
 
+    @IBOutlet weak var contentLeftMarginConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func onPanGesture(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: self.view)
+        let velocity = sender.velocity(in: self.view)
+        
+        if sender.state == UIGestureRecognizerState.began {
+            originalContentLeftMargin = contentLeftMarginConstraint.constant
+        } else if sender.state == UIGestureRecognizerState.changed {
+            contentLeftMarginConstraint.constant = originalContentLeftMargin + translation.x
+        } else if sender.state == UIGestureRecognizerState.ended {
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                if velocity.x > 0 {
+                    self.contentLeftMarginConstraint.constant = self.view.frame.width - 50
+                } else {
+                    self.contentLeftMarginConstraint.constant = 0
+                }
+                self.view.layoutIfNeeded()
+            })
+
+        }
     }
 }
